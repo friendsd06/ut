1506ebd83e1c70f307ad8cde9959b2401369817f
friendsd06 <friendsd06@gmail.com>
2024-10-21 16:42:25 +0530

    initial commit test27hhhhyy test-709g88--ooaddhgfh ---fsfds

--- src/re/null-nul-comparision/dbutils.py
+++ src/re/null-nul-comparision/dbutils.py
@@ -4,3 +4,13 @@ target_path = "s3a://your-bucket/path/to/data/partition45654/"
 
 # Use dbutils.fs.cp to copy files from source to target
 dbutils.fs.cp(source_path, target_path, recurse=True)
+
+
+def clean_redundant_nulls(df, columns):
+    for column in columns:
+        # Replace multiple ", , " with a single empty string
+        df = df.withColumn(column, regexp_replace(col(column), r"(, )+", ""))
+        # Remove leading or trailing commas
+        df = df.withColumn(column, regexp_replace(col(column), r"^,|,$", ""))
+
+    return df
