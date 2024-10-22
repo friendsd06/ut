89b436b2e888f4533b2c52dc0fb8bec09de85193
friendsd06 <friendsd06@gmail.com>
2024-10-23 01:14:14 +0530

    initial commit test27hhhhyy test-709g88--ooaddhgfh ---fsfds

--- src/re/directory-recon-huge-dataset-testing/drop-struct-array.py
+++ src/re/directory-recon-huge-dataset-testing/drop-struct-array.py
@@ -3,3 +3,10 @@ non_struct_array_cols = [field.name for field in df.schema.fields
 
 df_new = df.select(*non_struct_array_cols)
 df_new.printSchema()
+
+
+
+if nested_mismatch_col is not None:
+    difference_results[array_col] = difference_results[array_col].withColumn(
+        "nested_mismatch_col", nested_mismatch_col
+    )
