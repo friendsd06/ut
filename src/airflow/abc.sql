097268857aa581c0e5f4f7dfbbad311e8fc0c3c9
friendsd06 <friendsd06@gmail.com>
2024-11-10 12:49:22 +0530

    initial commit test27hhhhyy

---
+++ src/airflow/abc.sql
@@ -0,0 +1,8 @@
+-- Create the user if it doesn't exist
+CREATE USER myuser WITH PASSWORD 'mypassword';
+
+-- Grant privileges to the user
+ALTER USER myuser WITH SUPERUSER;
+GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+
+init-scripts/01-init.sql
