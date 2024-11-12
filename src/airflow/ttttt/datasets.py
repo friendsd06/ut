bc3e79e8f64e555623a109cde1ea3a8ce02cb8e4
friendsd06 <friendsd06@gmail.com>
2024-11-12 09:14:40 +0530

    initial commit test27hhhhyy

--- src/airflow/ttttt/datasets.py
+++ src/airflow/ttttt/datasets.py
@@ -1,8 +1,6 @@
-# dags/datasets.py
-
 from airflow.datasets import Dataset
 
-# Define a Dataset for each ingestion job with Windows file paths
-file1_dataset = Dataset("file:///D:/airflow-docker/dataset-path/file1.csv")
-file2_dataset = Dataset("file:///D:/airflow-docker/dataset-path/file2.csv")
-file3_dataset = Dataset("file:///D:/airflow-docker/dataset-path/file3.csv")
+# Define Datasets with container paths
+file1_dataset = Dataset("file:///opt/airflow/dataset-path/file1.csv")
+file2_dataset = Dataset("file:///opt/airflow/dataset-path/file2.csv")
+file3_dataset = Dataset("file:///opt/airflow/dataset-path/file3.csv")
