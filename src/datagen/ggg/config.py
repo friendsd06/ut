251ce3e7da49ae756c9b115da41a834d604e6c4c
friendsd06 <friendsd06@gmail.com>
2024-11-18 08:58:21 +0530

    initial commit test27hhhhyygg

--- src/datagen/ggg/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-db_config = {
-    'host': 'localhost',
-    'port': '5432',
-    'database': 'airflow',
-    'user': 'postgres',
-    'password': 'root'
-}
--- src/datagen/ggg/my_python_module.py
+++
@@ -1,5 +0,0 @@
-# my_python_module.py
-def my_python_callable(**kwargs):
-    print("Executing my_python_callable")
-    param1 = kwargs.get('param1')
-    print(f"param1: {param1}")
